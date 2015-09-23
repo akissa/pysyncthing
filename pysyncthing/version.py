2118dd7bc686fbc553b8dfeaeffee9485f811538
Andrew Colin Kissa <andrew@topdog.za.net>
2015-09-23 12:44:35 +0200

    * Added version file

---
+++ pysyncthing/version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+# pysyncthing Python bindings for Syncthing REST API
+# Copyright (C) 2015 Andrew Colin Kissa <andrew@topdog.za.net>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+pysyncthing: version
+"""
+
+version_info = (0, 0, 1)
+__version__ = ".".join(map(str, version_info))
